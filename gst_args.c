8fc1ea109df41316c4e476a83e7cc371fb045cc3
Edher <edher.carbajal@avnet.com>
2024-03-07 02:30:37 -0600

    gst stream working 2.0

--- gst_args.c
+++ gst_args.c
@@ -1,9 +1,5 @@
 #include <gst/gst.h>
 #include <string.h>
-#include <stdio.h>
-
-
-
 
 typedef struct _CustomData {
   gboolean is_live;
@@ -38,13 +34,10 @@ static void cb_message (GstBus *bus, GstMessage *msg, CustomData *data) {
       /* If the stream is live, we do not care about buffering. */
       if (data->is_live) break;
 
-      //break; //This Avoid the pipeline!!
-
       gst_message_parse_buffering (msg, &percent);
       g_print ("Buffering (%3d%%)\r", percent);
       /* Wait until buffering is complete before start/resume playing */
-      
-      if (percent < 10)
+      if (percent < 100)
         gst_element_set_state (data->pipeline, GST_STATE_PAUSED);
       else
         gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
@@ -62,6 +55,10 @@ static void cb_message (GstBus *bus, GstMessage *msg, CustomData *data) {
 }
 
 int main(int argc, char *argv[]) {
+
+  char *src_stream;
+  src_stream = argv[1];
+  //src_stream = "playbin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm";
   GstElement *pipeline;
   GstBus *bus;
   GstStateChangeReturn ret;
@@ -75,7 +72,7 @@ int main(int argc, char *argv[]) {
   memset (&data, 0, sizeof (data));
 
   /* Build the pipeline */
-  pipeline = gst_parse_launch ("playbin uri=https://rr4---sn-9gv7zn76.googlevideo.com/videoplayback?expire=1708045649&ei=8WDOZeX_Ltbgir4Pk-m3gAw&ip=168.195.202.11&id=o-AIT1v6H8Di3NaKAnxbUee1omptYtfPvm8fFjnEGS7ujV&itag=139&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&mh=uK&mm=31%2C26&mn=sn-9gv7zn76%2Csn-q4flrnl7&ms=au%2Conr&mv=m&mvi=4&pl=22&gcr=mx&initcwndbps=452500&vprv=1&mime=audio%2Fmp4&gir=yes&clen=710095&dur=116.160&lmt=1693248791517906&mt=1708023699&fvip=3&keepalive=yes&fexp=24007246&c=ANDROID_MUSIC&txp=2318224&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cvprv%2Cmime%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRQIhAMVF1t1xBLZaJxYK7FB-2GqH3qDnfcyoCRwtdM881u8OAiAzj9o9dFoTJERR2ZazfiLNUUo2f1YLFL7nvD8TOM80eA%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=APTiJQcwRQIgNc4qEcbG5TRC0VDlYYwRCeKCnMS4drmMSEB9e8-6gpICIQCBXj5Bwfkc3Ct2OUvHjqnP4XA60g3gbBIOuu7qkMgjlg%3D%3D", NULL);
+  pipeline = gst_parse_launch (src_stream, NULL);
   bus = gst_element_get_bus (pipeline);
 
   /* Start playing */
@@ -104,3 +101,4 @@ int main(int argc, char *argv[]) {
   gst_object_unref (pipeline);
   return 0;
 }
+
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 ### By xXxLord_Auch?xXx ######
 from ytmusicapi import YTMusic
-# import vlc
 from pytube import YouTube
-import time
+import os
 
 
 ######### Get all results for given search ########
@@ -11,8 +10,8 @@ import time
 ytmusic = YTMusic()
 
 # Search for something - only songs
-search_results_songs = ytmusic.search("kalo uchis moonlight", filter="songs")
-
+search_results_songs = ytmusic.search("Made it with chu", filter="songs")
+print("Results fetched...")
 # List results
 for count, element in enumerate(search_results_songs):
     print(f"\n{count} ---")
@@ -23,6 +22,7 @@ for count, element in enumerate(search_results_songs):
 
 # Youtube music video URL
 videoUrl = "https://www.youtube.com/watch?v="+search_results_songs[0]['videoId']
+print("YT URL music video fetched...")
 print(videoUrl)
 
 
@@ -38,3 +38,10 @@ print(stream_url)
 ###### STREAMING ######
 # (Playing media straight from the Internet without storing it locally is known as Streaming)
 
+###### Parse URL for Gstreamer ######
+gst_stream = './gst_args ' + '"playbin uri=' + stream_url + '"'
+print(gst_stream)
+
+##### Execute stream process ######
+# This calls a C routine that streams the URL fetched
+os.system(gst_stream)
